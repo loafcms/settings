0600b03e28d27d078db7aa3cbdc6fbfe507e06ab
Tom Lankhorst <info@tomlankhorst.nl>
2018-03-20 20:37:55 +0100

    Database resources

---
+++ database/factories/SettingFactory.php
@@ -0,0 +1,33 @@
+<?php
+
+$factory->define( \Loaf\Base\Models\Setting::class, function (Faker\Generator $faker) {
+
+    $attributes = [];
+
+    $path = [];
+    for( $i = 0; $i < $faker->numberBetween(1,3); $i++ )
+        $path[] = $faker->word;
+
+    $attributes['path'] = implode('.', $path );
+
+    $type = $faker->randomElement( ['string', 'integer', 'array'] );
+
+    $attributes['type'] = $type;
+
+    switch( $type )
+    {
+        case 'string':
+            $value = $faker->sentence;
+            break;
+        case 'integer':
+            $value = $faker->numberBetween;
+            break;
+        case 'array':
+            $value = $faker->words;
+    }
+
+    $attributes['value'] = $value;
+
+    return $attributes;
+    
+});
---
+++ database/migrations/2018_03_16_213000_create_settings_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSettingsTable extends Migration
+{
+    public function up()
+    {
+        Schema::create('settings', function (Blueprint $table) {
+            $table->increments('id');
+
+            $table->string('path', 255);
+            $table->string('scope', 255)->default( \Loaf\Base\Settings\SettingsManager::getDefaultScope() );
+
+            $table->timestamps();
+
+            // Store data in an efficient binary format if it's available
+            $table->jsonb('value')->nullable();
+            $table->string('type')->nullable();
+
+            // Composite primary key
+            $table->unique(['path', 'scope']);
+        });
+    }
+
+    public function down()
+    {
+        Schema::dropIfExists('settings');
+    }
+}
