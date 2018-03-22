6d44db2ab2dad5d9e2ca0daedceeadfd709b19fc
Tom Lankhorst <info@tomlankhorst.nl>
2018-03-22 14:13:43 +0100

    Language resources

---
+++ resources/lang/nl/settings.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    'edit settings' => ':Section instellingen',
+    'empty section' => 'lege sectie',
+    'this section is empty' => 'deze sectie is leeg',
+    'no editable settings' => 'deze groep heeft geen aanpasbare instellingen',
+];
